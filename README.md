ab72d799c36c68855f3c838a6038ef73c674f671
Pieter De Pauw <pieter.de.pauw@student.ehb.be>
2024-01-08 16:21:50 +0100

    fixed bug double sending request user controller. added info in README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # NodeJs-App
  Node and Express Api
+
+# Built with the use off
+https://medium.com/@kirankumal714/creating-a-powerful-restful-api-with-node-js-mysql-and-express-a-step-by-step-guide-18cdad7c6e18
+
+# Packages:
+express
+bcrypt
+JWT token
+mysql 2
+dotenv
+
+# Link to git
+https://github.com/PieterEhb/NodeJs-App
--- users/controller.js
+++ users/controller.js
@@ -6,17 +6,18 @@ export const createUser = async function (req, res) {
   let { username, password, email } = req.body;
   let sql = "select id from users where username = ? or email = ?";
   let [results] = await db_con.query(sql, [username, email]);
-  console.log(results);
+  //console.log(results);
   if (results[0] != null) {
     res.status(500).json({ message: "username or email already used!" });
-  }
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    if (err) throw err;
-    bcrypt.hash(password, salt, (err, hash) => {
+  } else {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
       if (err) throw err;
-      let sql = "insert into users(username, password, email) values(?,?,?)";
-      db_con.query(sql, [username, hash, email]);
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) throw err;
+        let sql = "insert into users(username, password, email) values(?,?,?)";
+        db_con.query(sql, [username, hash, email]);
+      });
     });
-  });
-  res.status(200).json({ message: "User Created" });
+    res.status(200).json({ message: "User Created" });
+  }
 };
